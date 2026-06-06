8b033f472770cd6f804725323fb2e9a7ac5139fe
Abhishek kumar <abhi060807@gmail.com>
2026-06-06 11:25:38 +0530

    updated frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config(); 
 
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./src/config/database');
-const leadRoutes = require('./src/routes/app');
-const errorHandler = require('./src/middleware/errorHandler');
+const app = require("./src/app");
 
-dotenv.config();
-connectDB();
+const leadRoutes = require("./src/routes/app");
+const errorHandler = require("./src/middleware/errorHandler");
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/leads', leadRoutes);
+app.use("/api/leads", leadRoutes);
 app.use(errorHandler);
 
-app.listen(3000,(req,res)=>{
-    console.log("server is running on 3000");
-    
-})
+const PORT =  3000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,20 @@
-const express = require("express")
-const cors = require("cors")
-const connecttodb = require("./config/database")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/database");
 
-app.use(express.json())
+const app = express();
 
-app.use(cors())
-connecttodb()
-module.exports = app
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+// React/Vite build serve karne ke liye
+app.use(express.static(path.join(__dirname, "..", "dist")));
+
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
+});
+
+module.exports = app;
